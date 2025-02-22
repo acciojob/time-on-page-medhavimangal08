dd8debf0794a2c398fb614618b4760cc37734b31
medhavimangal08 <medhavimangal43@gmail.com>
2025-02-22 18:19:26 +0530

    timeonpage

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  let [counter, setCounter] = useState(0);
+  useEffect(() => {
+    setInterval(() => {
+      setCounter((counter) => counter + 1);
+    }, 1000);
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      {counter > 0 && <p>You've been on this page for {counter} seconds</p>}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
